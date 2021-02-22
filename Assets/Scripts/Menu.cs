2005aa3c54919e782bdbbc85d9cfba0a48521af8
Eduardo Barbosa <eduardo_b_bosa@hotmail.com>
2021-02-21 22:30:20 -0300

    +Menu, MenuSound, SceneNames

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    public Sprite SoundON;
+    public Sprite SoundOFF;
+
+    GameObject mute;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mute = GameObject.Find("Mute");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        //GameObject.Find("GameManager").GetComponent<SoundManagerScript>().StopSound("MenuSound");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MuteSound()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            mute.GetComponent<Button>().GetComponent<Image>().sprite = SoundON;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            mute.GetComponent<Button>().GetComponent<Image>().sprite = SoundOFF;
+        }
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+    public AudioClip clip;
+    public bool loop;
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0, 256)]
+    public int priority;
+
+    public AudioSource audiosrc;
+
+    public void SetSource(AudioSource _audiosrc)
+    {
+        audiosrc = _audiosrc;
+        audiosrc.clip = clip;
+        audiosrc.loop = loop;
+        audiosrc.volume = volume;
+        audiosrc.priority = priority;
+    }
+
+    public void Play()
+    {
+        audiosrc.Play();
+    }
+
+    public void Stop()
+    {
+        audiosrc.Stop();
+    }
+
+
+}
+
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(GameObject.Find("SoundManager"));
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _sound = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _sound.transform.SetParent(this.transform);
+            sounds[i].SetSource(_sound.AddComponent<AudioSource>());
+        }
+        PlaySound("MenuSound");
+    }
+
+
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name.Equals(_name) && !sounds[i].audiosrc.isPlaying)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        // Debug.LogWarning("O nome " + _name + " não foi encontrado.");
+    }
+
+    public void StopSound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name.Equals(_name))
+            {
+                sounds[i].Stop();
+                return;
+            }
+        }
+
+        //Debug.LogWarning("O nome " + _name + " não foi encontrado.");
+    }
+
+}
